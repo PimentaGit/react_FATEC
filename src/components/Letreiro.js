01210ad3c1ac07163b21860bb5ee559d900e90a7
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-04-30 11:01:51 -0300

    Add files via upload

--- src/components/Letreiro.js
+++ src/components/Letreiro.js
@@ -1,21 +1,36 @@
 
-import Letreiro from ".components/Letreiro";
+import Letreiro from"/letreiro";
 
-function Letreiro() {              
-  return (
-    <div className="Letreiro">
-      <header className="Letreiro-header">
-        <img src={logo} className="Letreiro-logo" alt="letreiro" />
-        <p>
-          Edit <code>src/Letreiro.js</code> and save to reload.
-        </p>
-        
-       
-        
-      </header>
-    </div>
-  );
-}
+
+
+        function letreiroFrase(frase) {
+            const element = (
+                <h1>{frase}</h1>
+            )
+            ReactDOM.render(element, document.getElementById('root'))
+
+        }
+
+        let frase = "Venha estudar na FATEC!"
+        let cont = 0
+
+
+        let intervalo = setInterval(() => {
+
+            let temp = ""
+
+            for (let i = 0; i < cont; i++) {
+                temp += frase[i]
+            }
+            letreiroFrase(temp)
+            cont++
+
+            if (cont > frase.length) {
+                clearInterval(intervalo)
+            }
+        }, 500)
+
+
 
 export default Letreiro;
 
