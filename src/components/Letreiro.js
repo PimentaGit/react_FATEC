98f27738cafdf8e1c784151dd2feb70bf4cb81ac
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-04-30 09:31:39 -0300

    Add files via upload

---
+++ src/components/Letreiro.js
@@ -0,0 +1,21 @@
+
+import Projetos_react from"react";
+
+function Letreiro() {              
+  return (
+    <div className="Letreiro">
+      <header className="Letreiro-header">
+        <img src={logo} className="Letreiro-logo" alt="letreiro" />
+        <p>
+          Edit <code>src/Letreiro.js</code> and save to reload.
+        </p>
+        
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default Letreiro;
+
