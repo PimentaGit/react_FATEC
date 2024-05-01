596391b8b039f8cde60ed5deb5cb6734ee86f844
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-04-30 21:37:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';                       // importa o react para dentro da aplicação - local no node
-import ReactDOM from 'react-dom/client';         // para usar o virtualDom (vai direto no elemento)
-import './index.css';                            // importando o index.css - assim que eu importo um arquivo.
+import ReactDOM from 'react-dom/client';         // para usar o virtualDom (vai direto no elemento)                       // importando o index.css - assim que eu importo um arquivo.
 import MinhasRotas from "./minhasRotas";
-import Atv01 from './Atv01';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render (
