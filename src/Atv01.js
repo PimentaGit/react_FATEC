ab266d84c54dbe7c065c521da261e7066666ebae
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-05-07 19:16:23 -0300

    Add files via upload

--- src/Atv01.js
+++ src/Atv01.js
@@ -1,7 +1,8 @@
 import Relogio from './components/Relogio';
+import Letreiro from './components/Letreiro';
 import { Link } from "react-router-dom";
 
-export default function Atv01(){
+export default function Relogio(){
     return (
         
         <div>
@@ -14,3 +15,18 @@ export default function Atv01(){
        
     );
 }
+
+export default function Calendario(){
+    return (
+        
+        <div>
+            <h1>Atividade 02</h1>
+            <Letreiro/>
+            <br/>
+            <Link to="/">Retornar à página inicial</Link>
+        </div>
+        
+       
+    );
+}
+
