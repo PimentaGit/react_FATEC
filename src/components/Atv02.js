3f2886eb7f47ff79cab40ec49fcf0552eb254c79
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-05-07 20:10:16 -0300

    Add files via upload

---
+++ src/components/Atv02.js
@@ -0,0 +1,15 @@
+import Contador from './components/Contador';
+import { Link } from "react-router-dom";
+
+export default function Contador(){
+    return (
+        
+        <div>
+            <h1>Atividade 02 - Contador</h1>
+            <Letreiro/>
+            <br/>
+            <Link to="/">Retornar à página inicial</Link>
+        </div>
+        
+    );
+}
